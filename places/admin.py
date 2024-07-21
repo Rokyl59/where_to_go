b6e4e912d42c9b13f59fc2ac5859e36b0f83936d
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-07-21 18:38:53 +0500

    add admin

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
-
-
-@admin.register(PlaceImage)
-class PlaceImageAdmin(admin.ModelAdmin):
-    list_display = ['number', 'place']
+    inlines = [PlaceImageInline]
